06d51565c3bdb57eb0f2513502427576ae153074
lt177995 <tao.lv@ele.local>
2020-06-09 18:47:24 +0800

    create gui application with Wails

---
+++ gui/cpustats/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/Tutorial/gui/cpustats/sys"
+	"github.com/leaanthony/mewn"
+	"github.com/wailsapp/wails"
+)
+
+func basic() string {
+	return "Hello World!"
+}
+
+func main() {
+
+	js := mewn.String("./frontend/dist/app.js")
+	css := mewn.String("./frontend/dist/app.css")
+
+	stats := &sys.Stats{}
+
+	app := wails.CreateApp(&wails.AppConfig{
+		Width:  512,
+		Height: 512,
+		Title:  "CPU Usage",
+		JS:     js,
+		CSS:    css,
+		Colour: "#131313",
+	})
+	app.Bind(stats)
+	app.Run()
+}
