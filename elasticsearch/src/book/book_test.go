6c28f0ebabc789c06c5fc1330fd501cbbfe79a9b
lt177995 <tao.lv@ele-4.local>
2020-08-03 17:02:17 +0800

    add books testcase

--- elasticsearch/src/book/book_test.go
+++ elasticsearch/src/book/book_test.go
@@ -1,7 +1,10 @@
 package book
 
 import (
+	"context"
+	"elasticsearch/src/config"
 	"testing"
+	"time"
 )
 
 func TestGetBooks(t *testing.T) {
@@ -15,5 +18,47 @@ func TestGetBooks(t *testing.T) {
 		} else {
 			typeMap[t] = 1
 		}
+	}
+}
+
+func TestMarshal(t *testing.T) {
+	book := Book{ID: int64(1), Name: "Book", Type: "pdf", Size: 1024, Created: time.Now()}
+
+	str := Marshal(book)
+	if str == nil {
+		t.Fatal("Test Marshal book Failed.")
+	}
+
+	book2 := Unmarshal(str)
+	if book2 == nil {
+		t.Fatal("Test Unmarshal book Failed.")
+	}
+
+	if book2.ID != book.ID || book2.Name != book.Name || book.Size != book2.Size || book.Created != book2.Created {
+		t.Fatal("Test Unmarshal err.")
+	}
+}
+
+func TestGetBookWithID(t *testing.T) {
+	ctx := context.Background()
+
+	esConfig := config.GetEsInstance(ctx, Index)
+	defer esConfig.Stop()
+
+	book := GetBookWithID(ctx, esConfig, 1)
+	if book == nil {
+		t.Fatal("Test GetBookWithID Error.")
+	}
+}
+
+func TestGetBookWithName(t *testing.T) {
+	ctx := context.Background()
+
+	esConfig := config.GetEsInstance(ctx, Index)
+	defer esConfig.Stop()
+
+	books := GetBookWithName(ctx, esConfig, "1984")
+	if books == nil || len(books) == 0 {
+		t.Fatal("Test GetBookWithName Error.")
 	}
 }
