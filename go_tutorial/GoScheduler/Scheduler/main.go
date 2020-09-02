d45d5748b25737e7dc2e71f6b7cd2b794cd50add
lrtz <547617251lrtz@gmail.com>
2020-09-02 22:14:06 +0800

    test go concurrency

--- go_tutorial/GoScheduler/Scheduler/main.go
+++ go_tutorial/GoScheduler/Scheduler/main.go
@@ -3,12 +3,49 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
+	"sync/atomic"
 )
 
-func GetNumCPU() int {
+func getNumCPU() int {
 	return runtime.NumCPU()
 }
 
+func addConcurrent(goroutines int, numbers []int) int {
+	var v int64
+	totalNumbers := len(numbers)
+	lastGoroutine := goroutines - 1
+	stride := totalNumbers / goroutines
+
+	var wg sync.WaitGroup
+	wg.Add(goroutines)
+
+	for g := 0; g < goroutines; g++ {
+		go func(g int) {
+			start := g * stride
+			end := start + stride
+			if g == lastGoroutine {
+				end = totalNumbers
+			}
+
+			var lv int
+			for _, n := range numbers[start:end] {
+				lv += n
+			}
+
+			atomic.AddInt64(&v, int64(lv))
+			wg.Done()
+		}(g)
+	}
+
+	wg.Wait()
+
+	return int(v)
+}
+
 func main() {
-	fmt.Println(GetNumCPU())
+	fmt.Println(getNumCPU())
+
+	var numbers = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(addConcurrent(2, numbers))
 }
