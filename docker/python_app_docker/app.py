3a05259040ec466123a0e5cf3e79c81b32a4f16f
lrtz <547617251lrtz@gmail.com>
2020-10-27 18:22:54 +0800

    add expose port to Dockerfile

--- docker/python_app_docker/app.py
+++ docker/python_app_docker/app.py
@@ -1,19 +1,15 @@
-"""test python app run in docker."""
 
-import logging
-from flask import Flask, request
-import json
+from flask import Flask
+import socket
+import os
+
 app = Flask(__name__)
 
-logging.basicConfig(filename='./logs/log.log', level=logging.INFO)
-
-
-@app.route('/', methods=["POST"])
-def hello_world():
-    """hello_world test python app run in docker."""
-    data = request.get_data()
-    logging.info(data)
-    return json.dumps({"message": "success", "code": 200})
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+@app.route('/')
+def hello():
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
