3f503c5c16f70ef9017c10658470b0301585617f
tao.lv <547617251lrtz@gmail.com>
2022-02-18 17:29:00 +0800

    rust hello world

---
+++ Rust/the_rust_programming_language/1.Getting_Started/hello_cargo/README.md
@@ -0,0 +1,40 @@
+# Cargo
+
+- Cargo 是 Rust 的构建系统和包管理器
+- 可以处理构建代码、下载依赖库并编译等任务
+
+## 新建项目
+
+```bash
+cargo new hello_cargo
+```
+
+## 编译
+
+```bash
+cargo build
+```
+
+## 运行
+
+```bash
+./target/debug/hello_cargo
+```
+
+## 编译&运行
+
+```bash
+cargo run
+```
+
+## 代码检查
+
+```bash
+cargo check
+```
+
+## release
+
+```bash
+cargo build --release
+```
--- Rust/the_rust_programming_language/2.Programming_a_Guessing_Game/guessing_game/src/main.rs
+++ Rust/the_rust_programming_language/2.Programming_a_Guessing_Game/guessing_game/src/main.rs
@@ -5,23 +5,31 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    // 使用 let 语句来创建变量, 变量默认是不可变的; 在变量名前使用 mut 来使一个变量可变
+    let secret_number = rand::thread_rng().gen_range(0..101);
 
+    // loop 关键字创建了一个无限循环
     loop {
         println!("Please input your guess.");
+        // 创建了一个新的可变空字符串
         let mut guess = String::new();
 
         io::stdin()
+            // & 表示这个参数是一个引用（reference），它允许多处代码访问同一处数据，而无需在内存中多次拷贝
             .read_line(&mut guess)
             .expect("Failed to read line.");
 
+        // parse 方法 将字符串解析成数字，通过 let guess: u32 指定数字类型,guess 后面的冒号（:）告诉 Rust 我们指定了变量的类型
         let guess: u32 = match guess.trim().parse() {
+            // parse 返回一个 Result 类型，而 Result 是一个拥有 Ok 或 Err 成员的枚举
             Ok(num) => num,
+            // _ 是一个通配符值
             Err(_) => {
                 println!("Please type a number.");
                 continue;
             }
         };
+        // 花括号占位
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
@@ -29,7 +37,8 @@ fn main() {
             Ordering::Greater => println!("Too big."),
             Ordering::Equal => {
                 println!("you win!");
-                break;}
+                break;
+            }
         }
     }
 }
